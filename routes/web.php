2127ca50c4aa5265fad86fa19b286359525540fe
Kazi Abdul Halim <kazihalim44@gmail.com>
2026-03-21 23:46:03 +0600

    Integrated Laravel Breeze authentication and secured Order Management routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,33 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\OrderController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/order/create', [OrderController::class, 'create']);
-Route::post('/order/store', [OrderController::class, 'store']);
-Route::get('/orders', [OrderController::class, 'index']);
-Route::delete('/order/destroy/{id}', [OrderController::class, 'destroy']);
-Route::get('/order/edit/{id}', [OrderController::class, 'edit']);
-Route::put('/order/update/{id}', [OrderController::class, 'update']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 
+Route::middleware('auth')->group(function () {
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::get('/orders', [OrderController::class, 'index']);
+    Route::get('/order/create', [OrderController::class, 'create']);
+    Route::post('/order/store', [OrderController::class, 'store']);
+    Route::get('/order/edit/{id}', [OrderController::class, 'edit']);
+    Route::post('/order/update/{id}', [OrderController::class, 'update']);
+    Route::get('/order/delete/{id}', [OrderController::class, 'destroy']);
+
+});
+
+
+require __DIR__ . '/auth.php';
